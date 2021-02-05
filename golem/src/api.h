1f46797b7c39189024b5084c7d8d0acd8f57e7c4
David Sanchez <david.sanchez.falero@gmail.com>
2021-02-05 17:02:39 +0100

    first version added

---
+++ golem/src/api.h
@@ -0,0 +1,58 @@
+#include <WebServer.h>
+#include <M5Core2.h>
+#include <Arduino.h>
+
+struct Position{float x; float y; float z;};
+struct Golem{Position position;};
+enum PositionOptions {pos_X, pos_Y, pos_Z};
+Golem golem{{0.0, 0.0, 0.0}};
+WebServer golem_server(80);
+
+void handleRoot(){
+     golem_server.send(200, "text/plain", "Golem is ready!");
+}
+
+void handleNotFound(){
+    String message = "File Not Found\n\n";
+    message += "URI: ";
+    message += golem_server.uri();
+    message += "\nMethod: ";
+    message += (golem_server.method() == HTTP_GET) ? "GET" : "POST";
+    message += "\nArguments: ";
+    message += golem_server.args();
+    message += "\n";
+    for (uint8_t i = 0; i < golem_server.args(); i++){
+        message += " " + golem_server.argName(i) + ": " + golem_server.arg(i) + "\n";
+    }
+    golem_server.send(404, "text/plain", message);
+}
+
+PositionOptions resolvePositionOption(String input) {
+    if( input == "x" ) return pos_X;
+    if( input == "y" ) return pos_Y;
+    if( input == "z" ) return pos_Z;
+}
+
+void handleMove(){
+    for (uint8_t i = 0; i < golem_server.args(); i++){
+        switch (resolvePositionOption(golem_server.argName(i)))
+        {
+            case pos_X: golem.position.x = golem_server.arg(i).toFloat();
+            case pos_Y: golem.position.y = golem_server.arg(i).toFloat();
+            case pos_Z: golem.position.z = golem_server.arg(i).toFloat();
+        }
+    }
+    golem_server.send(200, "text/plain", "ok");   
+}
+
+void handleStop(){
+    golem_server.send(200, "text/plain", "Golem is waiting!");
+}
+
+void golem_api_setup(){
+    golem_server.on("/", handleRoot);
+    golem_server.on("/move", HTTP_POST, handleMove);
+    golem_server.on("/stop", handleStop);
+    golem_server.onNotFound(handleNotFound);
+    golem_server.begin();
+}
---
+++ golem/src/main.cpp
@@ -0,0 +1,49 @@
+#include <Arduino.h>
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <WebServer.h>
+#include <ESPmDNS.h>
+#include <M5Core2.h>
+#include "api.h"
+
+const char* ssid = "";
+const char* password = "";
+
+void wifi_setup(){
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    M5.Lcd.setCursor(0, 20);
+    M5.Lcd.printf("Connecting ...");
+  }
+  M5.Lcd.setCursor(0, 20);
+  M5.Lcd.printf("Connected to:        ");
+  M5.Lcd.setCursor(0, 45);
+  M5.Lcd.printf(ssid);
+  M5.Lcd.setCursor(0, 95);
+  M5.Lcd.printf("IP address:          ");
+  M5.Lcd.setCursor(0, 120);
+  char localIP[50];
+  WiFi.localIP().toString().toCharArray(localIP, 50);
+  M5.Lcd.printf(localIP);
+  MDNS.begin("esp32");
+}
+
+
+void setup(void) {
+  M5.begin();
+  M5.Lcd.fillScreen(BLACK);
+  M5.Lcd.setTextColor(GREEN , BLACK);
+  M5.Lcd.setTextSize(2);
+  wifi_setup();
+  golem_api_setup();
+  M5.Lcd.setCursor(0, 170);
+  M5.Lcd.printf("Golem server started");
+}
+
+void loop(void) {
+  golem_server.handleClient();
+  M5.Lcd.setCursor(0, 190);
+  M5.Lcd.printf("Position: %.02f %.02f %.02f", golem.position.x, golem.position.y, golem.position.z);
+}
